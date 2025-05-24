cb818392981c300b92af77e91576e0234102251b
mrcicil <rahmatkhairi91@gmail.com>
2025-05-24 15:35:51 +0800

    final edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import mockAPI from './api/mockapi'
 
-const datasetId = "d_a3c783f11d79ff7feb8856f762ccf2c5"
-const url = "https://api-open.data.gov.sg/v1/public/api/datasets/" + datasetId + "/poll-download";
-
-
 
 
 function App() {
+ async function apiGet() {
+        console.log("this is inside async");
+     try{
+        const response = await mockAPI.get("/environment/2-hour-weather-forecast");
+        console.log('GET status:', response.status);
+        console.log('GET data:', response.data);
+        setProduct(response.data);
+     } catch(error){
+        console.log(error.message);
+     }
 
-  async function apiGet(){
-    try {
-  let response = await fetch(url);
-  if (!response.ok) {
-    throw new Error('Failed to fetch poll-download data');
+      
   }
-  const jsonData = await response.json();
-  if (jsonData['code'] != 0) {
-    throw new Error(jsonData['errMsg']);
-  }
-  
-  const fetchUrl = jsonData['data']['url'];
-  response = await fetch(fetchUrl);
-  if (!response.ok) {
-    throw new Error('Failed to fetch inner data');
-  }
-  console.log(await response.text())
-} catch (e) {
-  console.error(e);
-}
-  }
-
 
   return (
     <div className="App">
-      <h1>Product List</h1>
-      <button onClick={apiGet}>Load Products</button>
+      <h1>Gov Data</h1>
+      <button onClick={apiGet}>Load Data</button>
     </div>
   );
 
 }
+
+
+
 
 export default App
--- src/api/mockapi.jsx
+++ src/api/mockapi.jsx
@@ -1,6 +1,7 @@
 //mockapi.jsx
 import axios from 'axios';
-const BASE_URL = 'https://api-production.data.gov.sg/v2/public/api/collections/1456https://api-open.data.gov.sg/v1/public/api/datasets/d_c00cbf056265738b684e63e890a113d2';
+const BASE_URL = 'https://api.data.gov.sg/v1';
 const mockAPI = axios.create({ baseURL: BASE_URL });
 
+//https://api-open.data.gov.sg/v2/real-time/api/two-hr-forecast
 export default mockAPI
